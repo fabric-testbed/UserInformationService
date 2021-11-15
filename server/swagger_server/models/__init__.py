9dc0cf73fef6c4db8e57bf59b7b1e1ffe6cf05fc
Ilya Baldin <ibaldin@renci.org>
2021-11-14 20:38:30 -0500

    Adding new models for v1.1

--- server/swagger_server/models/__init__.py
+++ server/swagger_server/models/__init__.py
@@ -7,10 +7,12 @@ from swagger_server.models.author_id import AuthorId
 from swagger_server.models.author_id_type import AuthorIdType
 from swagger_server.models.people_long import PeopleLong
 from swagger_server.models.people_short import PeopleShort
-from swagger_server.models.portal_key import PortalKey
 from swagger_server.models.preference_type import PreferenceType
 from swagger_server.models.preferences import Preferences
-from swagger_server.models.publication_full import PublicationFull
-from swagger_server.models.publication_short import PublicationShort
-from swagger_server.models.user_key import UserKey
+from swagger_server.models.ssh_key_bastion import SshKeyBastion
+from swagger_server.models.ssh_key_long import SshKeyLong
+from swagger_server.models.ssh_key_pair import SshKeyPair
+from swagger_server.models.ssh_key_short import SshKeyShort
+from swagger_server.models.ssh_key_status import SshKeyStatus
+from swagger_server.models.ssh_key_type import SshKeyType
 from swagger_server.models.version import Version
